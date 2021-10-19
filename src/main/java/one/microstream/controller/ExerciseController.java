b263fc79a33db7447319e1a11d45e95b1666a4ee
Kümmel Christian <c.kuemmel@microstream.one>
2021-10-19 23:32:37 +0200

    zc

--- src/main/java/one/microstream/controller/ExerciseController.java
+++ src/main/java/one/microstream/controller/ExerciseController.java
@@ -1,16 +1,11 @@
 package one.microstream.controller;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import one.microstream.domain.Address;
 import one.microstream.domain.Author;
 import one.microstream.domain.Book;
-import one.microstream.storage.DB;
 
 
 @Controller("/exercise")
@@ -19,38 +14,18 @@ public class ExerciseController
 	@Get("/booksfromfemaleauthor")
 	public List<Book> getBooksFromFemaleAuthor()
 	{
-		List<Book> collect =
-			DB.root.getBooks().stream().filter(b -> b.getAuthor().getGender().equalsIgnoreCase("female")).collect(
-				Collectors.toList());
-		return collect;
+		
 	}
 	
 	@Get("/authorsfrompoland")
 	public List<Author> getAuthorsFromPoland()
 	{
-		// return DB.root.getBooks().stream()
-		// .map(Book::getAuthor)
-		// .filter(author -> author.getAddresses().stream()
-		// .anyMatch(address -> address.getCountry().equals("Poland")))
-		// .collect(Collectors.toList());
 		
-		Predicate<Address> polandPredicate = a -> a.getCountry().equalsIgnoreCase("Poland");
-		
-		List<Author> collect = DB.root.getBooks().stream().map(b -> b.getAuthor()).filter(
-			a -> a.getAddresses().stream().anyMatch(polandPredicate)).collect(Collectors.toList());
-		
-		return collect;
 	}
 	
 	@Get("/booksreleasedbefore2012")
 	public List<Book> getBooksBefore2012()
 	{
-		List<Book> collect =
-			DB.root.getBooks().stream().filter(book -> book.getRelease().isBefore(LocalDate.of(2012, 1, 1))).collect(
-				Collectors.toList());
 		
-		DB.root.getBooks().stream().filter(b -> b.getRelease().getYear() < 2012).collect(Collectors.toList());
-		
-		return collect;
 	}
 }
